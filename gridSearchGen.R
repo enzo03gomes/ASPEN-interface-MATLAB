64c5f3db38409f0f81e0f211dcc4beb6bf98ffcc
Enzo Gomes <enzo03gomes@gmail.com>
2025-12-29 16:32:22 +0100

    Improved comments gridSearchGen.R

--- gridSearchGen.R
+++ gridSearchGen.R
@@ -1,6 +1,6 @@
 #---- SETUP ----
   #work directory
-  wdPath = dirname(rstudioapi::getActiveDocumentContext()$path)
+  wdPath = dirname(rstudioapi::getActiveDocumentContext()$path) #exchange this line with whatever directory you want
   setwd(wdPath)
   
   
@@ -8,11 +8,11 @@
   #name and path to file
   fileName = paste0(wdPath, "/gridSearchParameters.CSV")
   
-  #node names and values
+  #input node names and values
   nodes    = list("/Data/Blocks/TURB-1/Input/PRES" = seq(90, 200, length.out = 5),
                   "/Data/Blocks/TURB-2/Input/PRES" = seq(2, 30, length.out = 5))
   
-  #output names
+  #output leaf names
   output   = c("/Data/Flowsheeting Options/Calculator/EFFCALC/Output/WRITE_VAL/6")
 
   
@@ -32,4 +32,5 @@
   #save as CSV
   write.csv(gridMatrix, file = fileName)
   View(gridMatrix)
+
   
